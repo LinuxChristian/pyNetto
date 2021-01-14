76b2cead50547ed5bb91d5085f4fc83b945b7daf
Christian Juncker Braedstrup <christian@junckerbraedstrup.dk>
2021-01-14 17:41:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# pyNetto
+**pyNetto** is a small script that reads your Netto reciptes directly from your email and turns them into a Pandas Dataframe for further processing. The script contains some small examples of how to use the code.
+
+**NOTE**: This software is not associated with Netto in any way. It is provided under a GPLv3 license without any support. Please relate any issues with the App to [Netto](https://netto.dk/kundeservice/).
+
+## Getting started
+* Get the Netto Scan&Go app - [Android](https://play.google.com/store/apps/details?id=dk.dsg.scanandgo&hl=da_DK&gl=DK)/[iPhone](https://apps.apple.com/dk/app/netto-scan-go/id1424997991)
+* Setup the app to send reciptes to your email and go shopping
+* Install the Python dependencies required to run the script: `pip install -r requirements.txt`
+* If you are running the script directly update the constants on line 155-158 to match your setting
+* If you are importing the script in a notebook or seperate file read the 
+
+## Important variables
+These settings are dependent upon your email provider. You can likely find the settings for your provider on their website.
+
+- `USERNAME` : Your IMAP username (might be your email address)
+- `PASSWORD` : The password to your IMAP/email account
+- `IMAP_SERVER` : Your email server (e.g. `imap.google.com` or `imap.fastmail.com`) 
+- `EMAIL_FOLDER` : This should point to the folder where your emails are stored on the server. If all emails are in your inbox put `INBOX`.
+
+## Example output
+
+``` shell
+> python pyNetto.py
+
+------ Welcome to pyNetto ------
+Processed 22 emails from Netto
+
+You have in total (DKK) spent,
+6444.75
+
+
+Your most purchased product is,
+product              amount  price
+LA CAM HUMMUS SPICY      17  277.0
+
+
+Your most expensive product is,
+product             amount  price
+GÅRDKYL.FILET 280G       8  280.0
+```
---
+++ pyNetto.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+from datetime import datetime
+import quopri
+from bs4 import BeautifulSoup
+import pandas as pd
+import imaplib
+
+
+def convert_to_us_decimal(s):
+    """
+    Given a string with EU decimal formatting return a US formatted price
+    E.g. 3,00 -> 3.00
+
+    Parameters
+    ----------
+    s : str
+        Number as string to convert
+
+    Returns
+    -------
+    out : String with corrected decimal seperator
+    """
+    return s.replace(".", "").replace(",", ".")
+
+
+def connect_to_imap(server, user, passwd):
+    """Connect to IMAP server
+
+    Parameters
+    ----------
+    server : str
+        URL to the IMAP server (e.g. imap.google.com)
+    user : str
+        Username to the IMAP server
+    passwd : str
+        Password to the IMAP server
+
+    Returns
+    -------
+    out : Connection to the server
+    """
+    # create an IMAP4 class with SSL
+    imap = imaplib.IMAP4_SSL(server)
+    if user == "" or passwd == "":
+        print("Please update the script with our IMAP username and/or password")
+        exit()
+
+    # authenticate
+    imap.login(user, passwd)
+
+    return imap
+
+
+def process_emails(imap, folder):
+    """Process all emails from Netto in a specific folder
+
+    Parameters
+    ----------
+    imap : imaplib connection
+        Connection to your IMAP server. From the `connect_to_imap` function.
+    folder : str
+        Folder where you store your Netto emails (e.g. "INBOX")
+
+    Returns
+    -------
+    out : Pandas Dataframe with all your purchases. The dataframe contains the following
+        columns,
+
+        - time: The time of purchase
+        - product: Name of the product
+        - amount: Number of product purchases
+        - price: The price paid
+    """
+    # Get emails
+    status, messages = imap.select(folder, readonly=True)
+    status, messages = imap.search("utf-8", 'FROM', '"noreply@netto.dk"')
+
+    dfs = []
+    for msg in messages[0].split():
+        # Get the email body (i.e. content) and when it was sent
+        status, response = imap.fetch(msg, '(BODY[TEXT] INTERNALDATE)')
+        msg_info, msg_body = response[0]
+
+        # Get message datetime - E.g. 23-Jul-2020 12:02:04 -0400 -> local time
+        msg_time = imaplib.Internaldate2tuple(msg_info)
+        msg_time = datetime(msg_time.tm_year, msg_time.tm_mon, msg_time.tm_mday,
+                            msg_time.tm_hour, msg_time.tm_min, msg_time.tm_sec)
+
+        # Convert the email body encoded in quoted-printable to text
+        utf8_msg = quopri.decodestring(msg_body)
+
+        soup = BeautifulSoup(utf8_msg, features="lxml")
+
+        # the first argument to find tells it what tag to search for
+        # the second you can pass a dict of attr->value pairs to filter
+        # results that match the first tag
+        table = soup.find("table")
+
+        try:
+            rows = list()
+            for row in table.findAll("tr"):
+                if "items" in row.attrs["class"]:
+                    # The row (tr) has three data elemets (td)
+                    # Produce name - amount - price
+                    product, amount, price = row.findAll("td")
+                    clean_price = price.text.strip()
+
+                    # Handle bottle pant
+                    if "pant" in clean_price:
+                        # A product with pant is listed as: 4.50\n+ pant 3.00
+                        clean_price, pant_item = clean_price.split('\n+')
+                        pant_price = convert_to_us_decimal(pant_item.split('pant')[1].strip())
+
+                        # Add pant as seperate item
+                        rows.append({
+                            "time": msg_time,
+                            "product": "Pant",
+                            "amount": 1,
+                            "price": float(pant_price)
+                        })
+
+                    rows.append({
+                        # Remove newline charaters and make amount a number
+                        "time": msg_time,
+                        "product": product.text.strip(),
+                        "amount": int(amount.text.replace("stk", "").strip()),
+                        "price": float(convert_to_us_decimal(clean_price))
+                    })
+        except Exception as e:
+            raise RuntimeError(f"Error: unable to process item {row} from {msg_time}") from e
+
+        df = pd.DataFrame(rows)
+        dfs.append(df)
+
+    # Logout of email
+    imap.logout()
+
+    if len(dfs) == 0:
+        print(f"Error: could not find any emails from Netto in {EMAIL_FOLDER}")
+        exit()
+
+    # Combine into a single dataframe
+    ddf = pd.concat(dfs)
+    return ddf
+
+
+if __name__ == "__main__":
+    #
+    # UPDATE HERE WITH YOUR OWN SETTINGS
+    #
+    # Remember to update the values below to match your email provider
+    IMAP_SERVER = "imap.fastmail.com"
+    USERNAME = ''
+    PASSWORD = ''
+    EMAIL_FOLDER = "Netto"  # Email folder where you store your NETTO emails - E.g. INBOX
+
+    conn = connect_to_imap(IMAP_SERVER, USERNAME, PASSWORD)
+    df = process_emails(conn, EMAIL_FOLDER)
+
+    # The dataframe (df) contains the following columns,
+    # - time: The time of purchase
+    # - product: Name of the product
+    # - amount: Number of product purchases
+    # - price: The price paid
+
+    print("------ Welcome to pyNetto ------")
+    print(f"Processed {df.time.nunique()} emails from Netto\n")
+
+    # Products grouped by type - sum amount and price
+    unique_products = df.groupby('product').sum()
+
+    print("You have in total (DKK) spent,")
+    print(unique_products.price.sum())
+
+    print("\n\nYour most purchased product is,")
+    print(unique_products.sort_values(by='amount', ascending=False).head(1))
+
+    print("\n\nYour most expensive product is,")
+    print(unique_products.sort_values(by='price', ascending=False).head(1))
